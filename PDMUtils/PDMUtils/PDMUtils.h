8ec0a4337ade1de7bbf080a259aad663008e4787
pdiazmon <pedroluis.diaz@gmail.com>
2018-05-03 20:24:50 +0200

    First commit

---
+++ PDMUtils/PDMUtils/PDMUtils.h
@@ -0,0 +1,19 @@
+//
+//  PDMUtils.h
+//  PDMUtils
+//
+//  Created by Pedro L. Diaz Montilla on 3/5/18.
+//  Copyright © 2018 Pedro L. Diaz Montilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDMUtils.
+FOUNDATION_EXPORT double PDMUtilsVersionNumber;
+
+//! Project version string for PDMUtils.
+FOUNDATION_EXPORT const unsigned char PDMUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDMUtils/PublicHeader.h>
+
+
